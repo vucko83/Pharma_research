1c79d8fffa0b297bf89dcd5513a1d8f76eadcb95
vucko83 <vucko83@gmail.com>
2018-02-04 22:25:41 +0100

    External validations

--- Parameter_Builder.py
+++ Parameter_Builder.py
@@ -3,95 +3,18 @@ from sklearn.ensemble import GradientBoostingRegressor
 import numpy as np
 from sklearn.decomposition import NMF
 from sklearn.decomposition import PCA
+from sklearn.linear_model import Lasso
+from sklearn.svm import SVR
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import mutual_info_
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.linear_model import Ridge
 
 
-def create_models(n_samples, m_features):
-    random_forest_parameters = rf_parameters(n_samples, m_features)
-    gbt_parameters = rf_parameters(n_samples, m_features)
-    knn_parameters = {'n_neighbors': range(1, 11, 1)}
-    ann_parameters = {'learning_rate': ['constant'], 'learning_rate_init': [0.1, 0.2, 0.4, 0.6, 0.8, 0.9],
-                      'momentum': [0.1, 0.2, 0.4, 0.6, 0.8, 0.9], 'max_iter': [50000],
-                      'hidden_layer_sizes': nn_size(m_features)}
-    lasso_parameters = {'alpha': [0.1, 0.2, 0.4, 0.6, 0.8, 0.9], 'max_iter': [50000]}
-    lr_parameters = {'normalize': [True]}
-    svr_parameters = {'C': [1, 10, 100, 1000], 'kernel': ['linear', 'rbf']}
-
-
-'''
-Feature selectors
-
-'''
-
-def rf_parameters(n_samples, m_features, m_n_ratio=1):
-    random_forest_parameters = {
-        'n_estimators': range(10, 61, 20),
-        'max_features': n_features_range(n_samples, m_features),
-        'min_samples_leaf': np.arange(0.01, 0.06, 0.01),
-        'max_depth': range(2, 11, 2),
-    }
-
-    return (random_forest_parameters)
-
-
-
-
-
-def rf_gbt_param_dict(name='classify', estimators=[RandomForestRegressor(), GradientBoostingRegressor()], n_samples=100, m_features=15):
-    dict={
-        name: estimators,
-        name + '__' + 'n_estimators': range(10, 101, 10),
-        name + '__' + 'max_features': n_features_range(n_samples, m_features),
-        name + '__' + 'min_samples_leaf': np.arange(0.01, 0.06, 0.01),
-        name + '__' + 'max_depth': range(2, 11, 2),
-    }
-    return (dict)
-
-
-def create_params_PCA_NMF(name='reduce_dim', reducers=[PCA(), NMF()], n_samples=100, m_features=[3, 8, 16, 28] ):
-
-    params=[]
-    for m in m_features:
-        dict={
-            name: reducers,
-            name+'__'+'n_components':[m]
-        }
-        dict.update(rf_gbt_param_dict(m_features=m ))
-        params.append(dict)
-    return params
-
-
-a=create_params_PCA_NMF()
-a[0]
-
-
-'''
-C_OPTIONS = [1, 10, 100, 1000]
-param_grid = [
-    {
-        'reduce_dim': [PCA(), NMF()],
-        'classify': [RandomForestRegressor(), GradientBoostingRegressor()]a
-        'reduce_dim__n_components': N_FEATURES_OPTIONS,
-        'classify__C': C_OPTIONS
-    },
-    {
-        'reduce_dim': [PCA(), NMF()],
-        'reduce_dim__n_components': N_FEATURES_OPTIONS,
-        'classify__C': C_OPTIONS
-    },
-
-    {
-        'reduce_dim': [SelectKBest(mutual_info_regression)],
-        'reduce_dim__k': N_FEATURES_OPTIONS,
-        'classify__C': C_OPTIONS
-    }
-
-]
-
-'''
-
-
-
-def n_features_range(n_samples=100, m_features=2, m_n_ratio=1):
+def n_features_range(n_samples=100, m_features=3, m_n_ratio=1):
     '''
 
     :param n_samples: 
@@ -111,7 +34,7 @@ def n_features_range(n_samples=100, m_features=2, m_n_ratio=1):
 
     step_size = int(round(np.log2(max_features)))
 
-    param_range = list(range(min_features, max_features, step_size))
+    param_range = [a for a in range(min_features, max_features, step_size)]
 
     if param_range[-1] < max_features:
         param_range.append(max_features)
@@ -119,19 +42,123 @@ def n_features_range(n_samples=100, m_features=2, m_n_ratio=1):
     return param_range
 
 
-def rf_parameters(n_samples, m_features, m_n_ratio=1):
-    random_forest_parameters = {
-        'n_estimators': range(10, 61, 20),
-        'max_features': n_features_range(n_samples, m_features),
-        'min_samples_leaf': np.arange(0.01, 0.06, 0.01),
-        'max_depth': range(2, 11, 2),
-    }
-
-    return (random_forest_parameters)
-
 
 def nn_size(m_features):
     half = int(np.ceil((m_features + 1) / 2))
     quarter = int(np.ceil((m_features + 1) / 4))
     size = [(quarter,), (half,)]
     return (size)
+
+'''
+Feature selectors
+
+'''
+
+def lasso_param_dict(name='classify', estimators=[Lasso()], n_samples=100, m_features=15):
+    dict={
+        name: estimators,
+        name + '__' + 'alpha': [0.1, 0.2, 0.4, 0.6, 0.8, 0.9],
+        name + '__' + 'max_iter': [100000]
+    }
+    return (dict)
+
+def SVR_param_dict(name='classify', estimators=[SVR()], n_samples=100, m_features=15):
+    dict={
+        name: estimators,
+        name + '__' + 'C': [1, 10, 100, 500, 1000],
+        name + '__' + 'kernel': ['linear', 'rbf']
+    }
+    return (dict)
+
+def rf_param_dict(name='classify', estimators=[RandomForestRegressor()], n_samples=100, m_features=15):
+    dict={
+        name: estimators,
+        name + '__' + 'n_estimators': range(20, 101, 20),
+        name + '__' + 'max_features': n_features_range(n_samples, m_features),
+        name + '__' + 'min_samples_leaf': np.arange(0.01, 0.06, 0.01),
+        name + '__' + 'max_depth': range(2, 11, 2)
+    }
+    return (dict)
+
+def gbt_param_dict(name='classify', estimators=[GradientBoostingRegressor()], n_samples=100, m_features=15):
+    dict={
+
+        name: estimators,
+        name + '__' + 'n_estimators': range(10, 101, 20),
+        name + '__' + 'max_features': n_features_range(n_samples, m_features),
+        name + '__' + 'min_samples_leaf': np.arange(0.01, 0.06, 0.01),
+        name + '__' + 'max_depth': range(2, 11, 2)
+    }
+    return (dict)
+
+def knn_param_dict(name='classify', estimators=[KNeighborsRegressor()], n_samples=100, m_features=15):
+    dict={
+
+        name: estimators,
+        name + '__' + 'n_neighbors': range(1,11,1)
+    }
+    return (dict)
+
+
+def ann_param_dict(name='classify', estimators=[MLPRegressor()], n_samples=100, m_features=15):
+    dict={
+        name: estimators,
+        name + '__' + 'learning_rate': ['constant'],
+        name + '__' + 'momentum': [0.1, 0.2, 0.4, 0.6, 0.8, 0.9],
+        name + '__' + 'max_iter': [50000],
+        name + '__' + 'hidden_layer_sizes': nn_size(m_features)
+    }
+    return (dict)
+
+
+def lr_param_dict(name='classify', estimators=[LinearRegression()], n_samples=100, m_features=15):
+    dict={
+        name: estimators,
+        name + '__' + 'normalize': [False]
+    }
+    return (dict)
+
+
+def ridge_param_dict(name='classify', estimators=[Ridge()], n_samples=100, m_features=15):
+    dict={
+        name: estimators,
+        name + '__' + 'alpha': [0.1, 0.2, 0.4, 0.6, 0.8, 0.9],
+        name + '__' + 'max_iter': [100000]
+    }
+    return (dict)
+
+
+
+algorithms=[lasso_param_dict, SVR_param_dict, rf_param_dict, gbt_param_dict, knn_param_dict, ann_param_dict, lr_param_dict, ridge_param_dict]
+
+def create_params_pca_nmf(name='reduce_dim', reducers=[PCA(), NMF()], n_samples=100, m_features=[5, 10, 15, 20, 25, 30], funcs=[]):
+    params=[]
+
+    for func in funcs:
+        for m in m_features:
+            dict = {
+                name: reducers,
+                name+'__'+'n_components':[m]
+            }
+            dict.update(func(m_features=m))
+
+            params.append(dict.copy())
+    return (params)
+
+
+def create_params_k_best(name='reduce_dim', reducers=[SelectKBest()], n_samples=100, m_features=[5, 10, 15, 20, 25, 30], funcs=[]):
+    params=[]
+    for func in funcs:
+        for m in m_features:
+            dict = {
+                name: reducers,
+                name+'__'+'k':[m]
+            }
+            dict.update(func(m_features=m))
+
+            params.append(dict.copy())
+    return (params)
+
+
+params_dicts=create_params_pca_nmf(funcs=algorithms)
+param_dicts_all=params_dicts+create_params_k_best(funcs=algorithms)
--- Pipelined_training.py
+++ Pipelined_training.py
@@ -3,6 +3,9 @@ from sklearn.pipeline import Pipeline
 from sklearn.decomposition import PCA, NMF
 import pandas as pd
 import numpy as np
+from sklearn.feature_selection import f_regression
+from sklearn.feature_selection import mutual_info_regression
+
 
 #http://scikit-learn.org/stable/modules/pipeline.html
 import numpy as np
@@ -22,67 +25,29 @@ data=pd.read_csv('Aripiprazol.csv')
 def prepare_data(data):
     Y=data.iloc[:,-1] # label
     X=data.iloc[:,:-1] # input data
-    #scaler = StandardScaler()
-    #scaler.fit(X)
-    #X = scaler.transform(X)
     n_samples = X.shape[0]
     m_features = X.shape[1]
     return (X, Y, n_samples, m_features)
 
-'''
-pca=PCA(n_components=2)
-model=pca.fit_transform(X)
 
-model.
-X,y, n_samples, m_features=prepare_data(data)
-X.describe
-
-'''
-from sklearn.ensemble import IsolationForest
 X,y, n_samples, m_features=prepare_data(data)
 
 pipe = Pipeline([
     ('normalize', MinMaxScaler()),
-    #('outliers', IsolationForest),
     ('reduce_dim', PCA()),
     ('classify', SVR())
 ])
 
-from sklearn.feature_selection import f_regression
-from sklearn.feature_selection import mutual_info_regression
 
 
-'''
-N_FEATURES_OPTIONS = [2, 4, 8, 16, 28]
-C_OPTIONS = [1, 10, 100, 1000]
-param_grid = [
-    {
-        # 'reduce_dim': [PCA(iterated_power=7), NMF()],
-        'reduce_dim': [PCA(), NMF()],
-        'reduce_dim__n_components': N_FEATURES_OPTIONS,
-        'classify__C': C_OPTIONS
-    },
-
-    {
-        'reduce_dim': [SelectKBest(mutual_info_regression)],
-        'reduce_dim__k': N_FEATURES_OPTIONS,
-        'classify__C': C_OPTIONS
-    }
-
-]
-reducer_labels = ['PCA', 'NMF', 'KBest(chi2)']
-
-'''
-
-
-
-
-scoring = ['neg_mean_squared_error', 'r2']
-grid = GridSearchCV(pipe, cv=10, scoring=scoring, refit=scoring[0], n_jobs=2, param_grid=a, return_train_score=True)
+scoring = ['neg_mean_squared_error', 'r2', 'explained_variance', 'neg_mean_absolute_error','neg_median_absolute_error']
+grid = GridSearchCV(pipe, cv=10, scoring=scoring, refit=scoring[0], n_jobs=4, param_grid=param_dicts_all, return_train_score=True)
 grid.fit(X, y).predict(X)
-grid.cv_results_
 
+df=pd.DataFrame(grid.cv_results_)
+df.to_csv('test_results.csv')
 
+'''
 X.describe()
 grid.best_estimator_
 from sklearn.model_selection import cross_val_predict
@@ -91,6 +56,3 @@ np.average(cross_val_score(grid.best_estimator_,X,y=y,scoring='r2', n_jobs=4, cv
 
 
 '''
-
-
-  '''
