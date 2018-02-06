0c9519c619ba66032fe69712a996de8315a5ccd4
vucko83 <vucko83@gmail.com>
2018-02-06 17:35:19 +0100

    External validations

--- Plotting_Distributions.py
+++ Plotting_Distributions.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 data=pd.read_csv('Aripiprazol.csv')
 
 plt.plot(data['k'])
 data.plot(kind='line')
 data.k.plot(kind='hist')
+
+plt.hist(np.log(data.k))
